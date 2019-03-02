a501eaca5df238aa1a20d7dbd3a012031ea8a770
Eduardo <eduardo.garcia@magmalabs.io>
2019-03-02 13:51:22 -0600

    Add Class to get all news_urls into a web site

---
+++ news_page.rb
@@ -0,0 +1,33 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class NewsPage
+  attr_accessor :root_url, :urls
+
+  def initialize(root_url)
+    @root_url = root_url
+    @urls = []
+  end
+
+  def find_articles(page: nil)
+    @current_page = page
+    visit_page.css('article').map { |item| item.at_css('div.card-img a').values }
+  end
+
+  def visit_page
+    Nokogiri::HTML(open("#{@root_url}page/#{@current_page}"))
+  end
+
+  def visit_all_pages(limit: nil)
+    loop.with_index do |_, index|
+      @urls = @urls + find_articles(page: index+1)
+      puts @urls.last
+      puts @urls.count
+      puts index
+      break unless visit_page
+      break unless index < limit && !limit.nil? 
+    end
+  end
+
+end
---
+++ news_page_spec.rb
@@ -0,0 +1,21 @@
+require File.join(File.dirname(__FILE__), 'news_page')
+
+describe NewsPage do
+  let(:news_page) { NewsPage.new(root_url) }
+  let(:root_url) { 'https://periodicocorreo.com.mx/category/leon/' }
+
+  describe '#find_articles' do
+    subject { news_page.find_articles(page: page) }
+
+    let(:page) { 1 }
+    it 'returns an array with all urls founded' do
+      expect(subject).to be_a Array
+    end
+  end
+
+  describe '#visit_all_pages' do
+    it ' ' do
+      binding.pry
+    end
+  end
+end
