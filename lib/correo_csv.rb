cf46291ed5f081c4ee4c76795a1336c078ee8c83
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2019-03-07 19:29:42 -0600

    Add tests for csv generation class

--- lib/correo_csv.rb
+++ lib/correo_csv.rb
@@ -2,26 +2,28 @@ require 'csv'
 require_relative 'news'
 
 class CorreoCsv
-  def call(urls)
-    rows = urls.map do |url|
-      News.new(url).csv_row
-    end
+  HEADER = %w[Título Fecha Autor Tags Contenido URL].freeze
+  def initialize(urls)
+    @urls = urls
+  end
+  def generate
+    CSV.open('./periodico_correo.csv', 'wb') do |csv|
+      csv << HEADER
 
-    CSV.open("./periodico_correo.csv", "wb") do |csv|
-      csv << ['titulo', 'Fecha', 'Autor', 'Tags', 'Contenido', 'URL']
+      # FIXME: I guess should be a better way to do this insertion
       rows.each do |row|
         csv << row
       end
     end
   end
+
+  private
+
+  attr_reader :urls
+
+  def rows
+    urls.map do |url|
+      News.new(url).csv_row
+    end
+  end
 end
-
-# TODO change this with the class that manages the news links
-urls =['https://periodicocorreo.com.mx/niega-la-salle-acceso-al-area-afectada-por-tala',
- 'https://periodicocorreo.com.mx/es-el-arroyo-de-alfaro-un-vertedero-de-basura/',
- 'https://periodicocorreo.com.mx/detecta-implan-52-mil-casas-abandonadas-en-la-ciudad/',
- 'https://periodicocorreo.com.mx/leoneses-desconocen-los-centros-de-atencion-a-victimas/',
- 'https://periodicocorreo.com.mx/construiran-25-kilometros-mas-de-ciclovias/'
-]
-
-CorreoCsv.new.(urls)
--- spec/correo_csv_spec.rb
+++ spec/correo_csv_spec.rb
@@ -1,5 +1,27 @@
 require_relative '../lib/correo_csv'
 
 describe CorreoCsv do
-  # TODO
+  describe '#generate' do
+    subject { described_class.new(urls).generate }
+
+    let(:urls) do
+      %w[https://juancrg90.me https://juanruiz.dev]
+    end
+
+    let(:news) { instance_double('News', csv_row: %w[Lorem ipsum dolor sit amet]) }
+
+    it 'generates a csv with news rows' do
+      allow(News).to receive(:new).and_return(news)
+
+      subject
+
+      csv = CSV.read('./periodico_correo.csv')
+
+      aggregate_failures do
+        expect(csv.count).to eq(3)
+        expect(csv.first).to eq(%w[Título Fecha Autor Tags Contenido URL])
+        expect(csv.last).to eq(%w[Lorem ipsum dolor sit amet])
+      end
+    end
+  end
 end
